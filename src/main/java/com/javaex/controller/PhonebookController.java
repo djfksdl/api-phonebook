f90615822687b72abffb54d69da7a47d9e5f9332
djfksdl <djfksdl@naver.com>
2024-03-26 17:27:45 +0900

    list불러오기

--- src/main/java/com/javaex/controller/PhonebookController.java
+++ src/main/java/com/javaex/controller/PhonebookController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.javaex.service.PhonebookService;
@@ -16,7 +17,7 @@ public class PhonebookController {
 	@Autowired
 	private PhonebookService phonebookService;
 	
-	//test
+	//전체 리스트 불러오기
 	@GetMapping("/api/phonebooks")
 	public List<PersonVo> list() {
 		System.out.println("PhonebookController.list");
@@ -26,4 +27,13 @@ public class PhonebookController {
 		
 		return phonebookList;
 	}
+	
+	//수정폼
+	@GetMapping("/api/phonebooks/modify/{personId}")
+	public String modify() {
+		System.out.println("PhonebookController.modify");
+		return "";
+	}
+	
+	//수정하기
 }
