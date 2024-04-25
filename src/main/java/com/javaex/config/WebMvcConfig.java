848b9caf4cf646bc4f3cd160bf2842285c3f57f0
djfksdl <djfksdl@naver.com>
2024-04-25 17:55:22 +0900

    주소*

--- src/main/java/com/javaex/config/WebMvcConfig.java
+++ src/main/java/com/javaex/config/WebMvcConfig.java
@@ -9,6 +9,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/api/**") // 경로
-				.allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("*");
+				.allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("http://localhost:8080");//flutter에서 안되면 allowedOrigins주소를 *로 바꾸면 된다. 
 	}
 }
